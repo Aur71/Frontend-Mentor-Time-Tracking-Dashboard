d45a658734286bc945fea1190f1a0d7fb8b89ff3
Aur71 <111057774+Aur71@users.noreply.github.com>
2022-08-24 13:22:35 +0300

    Design done

---
+++ app.js
@@ -0,0 +1,4 @@
+const btn = document.querySelectorAll("[data-status]");
+const h2 = document.querySelectorAll("h2");
+const h3 = document.querySelectorAll("h3");
+const span = document.querySelectorAll("span")
